99d568534a71a3dc2951966f5cd6a1ac04551356
ahxdevelopmx <ah@xdevelop.mx>
2022-01-14 10:40:10 -0600

    cambios

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,34 +6,56 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
-export class PorPaisComponent{
+export class PorPaisComponent {
 
-  termino : string = '';
+  termino: string = '';
   hayError: boolean = false;
-  paises  : Country[] = [];
+  paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
-  buscar(termino:string){
+  buscar(termino: string) {
     this.hayError = false;
+    this.mostrarSugerencias = false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
       .subscribe({
-        next: (paises)=>{
+        next: (paises) => {
           console.log(paises);
           this.paises = paises;
         },
-        error: (err)=>{
+        error: (err) => {
           this.hayError = true;
           this.paises = [];
         }
       })
   }
 
-  sugerencias(termino: string){
+  sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarPais(termino)
+      .subscribe({
+          next: paises => {
+            this.paisesSugeridos = paises.splice(0, 5);
+          },
+          error: err=>{
+            this.paisesSugeridos = [];
+          }
+      })
   }
 
+  buscarSugerido(termino: string){
+    this.buscar(termino);
+    
+  }
 }
